cc14876e53af4a9ee69645e55bcaca378244ef37
mauroanastasi <mauroanastasi1@gmail.com>
2025-07-18 19:50:53 +0200

    [UPD] fetch call with userId at chef information

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const getChefBirthday = async (id) => {
     const ric = await fetch(`https://dummyjson.com/recipes/${id}`)
     const ricetta = await ric.json()
-    return ricetta
+    const chef = await fetch(`https://dummyjson.com/users/${ricetta.userId}`)
+    const idChef = chef.json()
+    return idChef
+
 }
 
 (async () => {
-    const ricetta = await getChefBirthday(3)
+    const ricetta = await getChefBirthday(1)
     console.log(ricetta)
 })();
