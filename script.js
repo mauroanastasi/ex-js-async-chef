27fefdd38f20e921a05859ba8dfd705f70194c86
mauroanastasi <mauroanastasi1@gmail.com>
2025-07-19 03:09:43 +0200

    [UPD] add specific error for birthDate undefined (id wrote) and fetch's calls

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const getChefBirthday = async (id) => {
-    const ric = await fetch(`https://dummyjson.com/recipes/${id}`)
-    const ricetta = await ric.json()
-    const chef = await fetch(`https://dummyjson.com/users/${ricetta.userId}`)
-    const idChef = await chef.json()
+
+    let ric
+    let ricetta
+
+    try {
+        ric = await fetch(`https://dummyjson.com/recipes/${id}`)
+        ricetta = await ric.json()
+
+    } catch (error) {
+        throw new Error(`Non è stato possibile trovare la ricetta con id ${id}`)
+    }
+
+    let chef
+    let idChef
+
+
+    try {
+        chef = await fetch(`https://dummyjson.com/users/${ricetta.userId}`)
+        idChef = await chef.json()
+
+    } catch (error) {
+        throw new Error(`Non è stato possibile trovare lo chef con id ${ricetta.userId}`)
+
+    }
     return idChef.birthDate
 
 }
 
 (async () => {
-    const birthDate = await getChefBirthday(1)
-    console.log(birthDate)
+    try {
+        const birthDate = await getChefBirthday(3)
+        console.log(`Ecco la data di compleanno dello chef:`, birthDate)
+
+        if (birthDate === undefined) {
+            throw new Error(`Hai inserito un id non valido`)
+        }
+
+    } catch (error) {
+        console.error(error)
+
+    } finally {
+        console.log(`Fine operazione!`)
+    }
 })();
